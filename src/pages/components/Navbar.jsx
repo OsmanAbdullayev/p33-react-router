b36c9b31c65c72684e17714d90fc4a81b85e6e2f
Osman Abdullayev <os.abdullayev@gmail.com>
2022-10-26 01:38:29 +0400

    after updating README and fixing active links

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,58 +1,57 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
 	return (
 		<nav className="navbar navbar-expand-lg bg-white navbar-light shadow-sm py-3 py-lg-0 px-3 px-lg-0">
-			<Link to="/" className="navbar-brand ms-lg-5">
+			<NavLink to="/home" className="navbar-brand ms-lg-5">
 				<h1 className="m-0 text-uppercase text-dark">
 					<i className="bi bi-shop fs-1 text-primary me-3" />
 					Pet Shop
 				</h1>
-			</Link>
+			</NavLink>
 			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
 				<span className="navbar-toggler-icon" />
 			</button>
 			<div className="collapse navbar-collapse" id="navbarCollapse">
 				<div className="navbar-nav ms-auto py-0">
-					<Link to="/" className="nav-item nav-Link active">
+					<NavLink to="/home" className="nav-item nav-link">
 						Home
-					</Link>
-					<Link to="/about" className="nav-item nav-Link">
+					</NavLink>
+					<NavLink to="/about" className="nav-item nav-link">
 						About
-					</Link>
-					<Link to="/service" className="nav-item nav-Link">
+					</NavLink>
+					<NavLink to="/service" className="nav-item nav-link">
 						Service
-					</Link>
-					<Link to="/product" className="nav-item nav-Link">
+					</NavLink>
+					<NavLink to="/product" className="nav-item nav-link">
 						Product
-					</Link>
+					</NavLink>
 					<div className="nav-item dropdown">
-						<Link to="#" className="nav-Link dropdown-toggle" data-bs-toggle="dropdown">
+						<NavLink to="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
 							Pages
-						</Link>
+						</NavLink>
 						<div className="dropdown-menu m-0">
-							<Link to="/price" className="dropdown-item">
+							<NavLink to="/price" className="dropdown-item">
 								Pricing Plan
-							</Link>
-							<Link to="/teams" className="dropdown-item">
+							</NavLink>
+							<NavLink to="/teams" className="dropdown-item">
 								The Team
-							</Link>
-							<Link to="/testimonials" className="dropdown-item">
+							</NavLink>
+							<NavLink to="/testimonials" className="dropdown-item">
 								Testimonial
-							</Link>
-							<Link to="/blogs" className="dropdown-item">
+							</NavLink>
+							<NavLink to="/blogs" className="dropdown-item">
 								Blog Grid
-							</Link>
-							<Link to="/details" className="dropdown-item">
+							</NavLink>
+							<NavLink to="/details" className="dropdown-item">
 								Blog Detail
-							</Link>
+							</NavLink>
 						</div>
 					</div>
-					<Link to="#" className="nav-item nav-Link nav-contact bg-primary text-white px-5 ms-lg-5">
+					<NavLink to="#" className="nav-item nav-link nav-contact bg-primary text-white px-5 ms-lg-5">
 						Contact <i className="bi bi-arrow-right" />
-					</Link>
+					</NavLink>
 				</div>
 			</div>
 		</nav>
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -22,7 +22,7 @@ function App() {
         <Topbar />
         <Navbar />
 			<Routes>
-				<Route path="/" element={<Home />}></Route>
+				<Route path="/home" element={<Home />}></Route>
 				<Route path="/about" element={<About />}></Route>
 				<Route path="/service" element={<Service />}></Route>
 				<Route path="/product" element={<Product />}></Route>
